50ed80ae9ab71fd60ef4c1afa04e9ee8d756b9e1
aliaksei-andreyeu <aliaksei.andreyeu@gmail.com>
2023-11-16 10:20:40 +0300

    Submit M4.1T5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int result = year%4;
+        if (year%100==0 && year%4==0) {
+            if (year%400==0){
+                result=0;
+            } else {
+                result=1;
+            }
+        }
+        switch (result) {
+            case (0):
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
